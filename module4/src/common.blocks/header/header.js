55803273db26ca8e9ebaa8c73462acf5b96c9bfe
gruv19 <grv1992@yandex.ru>
2022-05-09 23:38:40 +0500

    feat: Добавил блок 'header'

---
+++ module4/src/common.blocks/header/header.js
@@ -0,0 +1,44 @@
+import './header.scss';
+import headerTemplate from './header.template';
+
+import Logo from '../../library.blocks/logo/logo';
+import BurgerButton from '../../library.blocks/burger-button/burger-button';
+import Nav from '../../library.blocks/nav/nav';
+
+class Header {
+  constructor() {
+    this.element = null;
+    this.logo = new Logo();
+    this.burger = new BurgerButton();
+    this.nav = new Nav();
+  }
+
+  getElement() {
+    if (!this.element) {
+      const temporaryElement = document.createElement('div');
+      temporaryElement.innerHTML = headerTemplate();
+      this.element = temporaryElement.firstElementChild;
+      this.element.querySelector('.header__logo').append(this.logo.getElement());
+      this.element.querySelector('.header__burger').append(this.burger.getElement());
+      this.element.querySelector('.header__nav').append(this.nav.getElement());
+    }
+    return this.element;
+  }
+
+  setMenuHandler() {
+    this.burger.getElement().addEventListener('click', this.mobileNavHandler.bind(this));
+  }
+
+  mobileNavHandler() {
+    if (this.burger.getState()) {
+      this.burger.setStateFalse();
+      this.nav.getElement().parentElement.classList.remove('header__nav--active');
+    } else {
+      this.burger.setStateTrue();
+      this.nav.getElement().parentElement.classList.add('header__nav--active');
+    }
+  }
+
+}
+
+export default Header;
---
+++ module4/src/common.blocks/header/header.template.js
@@ -0,0 +1,10 @@
+const headerTemplate = () => (`<header class="header">
+  <div class="header__logo">
+  </div>
+  <div class="header__burger">
+  </div>
+  <div class="header__nav">
+  </div>
+</header>`)
+
+export default headerTemplate;
