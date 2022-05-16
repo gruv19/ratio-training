1e6f6de4a81b8e3741710bb1a9d6d9df32a6c0d0
gruv19 <grv1992@yandex.ru>
2022-05-16 21:04:26 +0500

    feat: Добавил вставку данных в шаблон, полученный извне

--- module4/src/common.blocks/article/article.js
+++ module4/src/common.blocks/article/article.js
@@ -2,15 +2,16 @@ import './article.scss';
 import articleTemplate from './article.template';
 
 class Article {
-  constructor(type = 'default') {
+  constructor(content, type = 'default') {
     this.type = type;
     this.element = null;
+    this.content = content;
   }
 
   getElement() {
     if (!this.element) {
       const temporaryElement = document.createElement('div');
-      temporaryElement.innerHTML = articleTemplate(this.type);
+      temporaryElement.innerHTML = articleTemplate(this.content, this.type);
       this.element = temporaryElement.firstElementChild;
     }
     return this.element;
--- module4/src/common.blocks/article/article.template.js
+++ module4/src/common.blocks/article/article.template.js
@@ -1,19 +1,22 @@
-const articleTemplate = (type) => (`<article class="article ${(type != 'default') ? `article--${type}` : ''}">
-<img src="https://naked-science.ru/wp-content/uploads/2016/04/article_Vehicle-Prototype-Image-Banner-Cropped-600px-1200x796.jpg" alt="" class="article__img">
-<div class="article__content">
-  <p class="article__tag">FEATURED ARTICLE</p>
-  <h1 class="article__title">World’s Most Dangerous Technology Ever Made.</h1>
-  <div class="article__meta">
-    <span class="article__author">Leslie Pena</span>
-    <span class="article__divider"></span>
-    <span class="article__date-time">April 25, 2012 (6 mins read)</span>
+import { formatDate, formatReadTime } from "../../utils";
+
+const articleTemplate = (content, type) => {
+  const { id, tag, title, author, createdAt, image, readTime, description } = content;
+  return (`<article class="article ${(type != 'default') ? `article--${type}` : ''}">
+  ${(type === 'default') ? `<a class="article__link" href="/blog/article/${id}">` : ''}
+    <img src="${image}" alt="" class="article__img">
+  ${(type === 'default') ? `</a>` : ''}
+  <div class="article__content">
+    <p class="article__tag">${tag}</p>
+    <h1 class="article__title"><a class="article__link" href="/blog/article/${id}">${title}</a></h1>
+    <div class="article__meta">
+      <span class="article__author">${author}</span>
+      <span class="article__divider"></span>
+      <span class="article__date-time">${formatDate(createdAt)} (${formatReadTime(readTime)} mins read)</span>
+    </div>
+    <p class="article__text">${description}</p>
   </div>
-  <p class="article__text">
-    Proident aliquip velit qui commodo officia qui consectetur dolor ullamco aliquip elit incididunt.
-    Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint consectetur Lorem amet aliqua do enim.
-    Commodo duis dolor anim excepteur. In aliquip mollit nulla consequat velit magna.
-  </p>
-</div>
-</article>`);
+  </article>`)
+};
 
 export default articleTemplate;
