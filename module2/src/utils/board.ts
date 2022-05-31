ebf43b2f36787f05c2953b260ecf210c06fdd425
gruv19 <grv1992@yandex.ru>
2022-05-31 23:38:20 +0500

    feat: Добавил типы данных и убрал проверку ввода имени пользователя до начала игры

--- module2/src/utils/board.ts
+++ module2/src/utils/board.ts
@@ -2,48 +2,96 @@ import Board from "../components/board/board";
 import Cell from "../components/cell/cell";
 import { boardInitParams } from '../const';
 
-function setKeyDownHandler(leftKeyHandler: Function, upKeyHandler: Function, rightKeyHandler: Function, downKeyHandler: Function, thisArg: Board) {
+function setKeyDownHandler(leftKeyHandler: Function, upKeyHandler: Function, rightKeyHandler: Function, downKeyHandler: Function, thisArg: Board): void {
   document.addEventListener('keydown', (event) => {
     if (event.keyCode === 37) {
-      if (thisArg.getGameUsername()) {
-        event.preventDefault();
-        leftKeyHandler.apply(thisArg);
-      } else {
-        if (thisArg.getGameUsernameInput() !== document.activeElement) {
-          alert('Необходимо ввести имя игрока');
-        }
-      }
+      event.preventDefault();
+      leftKeyHandler.apply(thisArg);
     } else if (event.keyCode === 38) {
-      if (thisArg.getGameUsername()) {
-        event.preventDefault();
-        upKeyHandler.apply(thisArg);
-      } else {
-        if (thisArg.getGameUsernameInput() !== document.activeElement) {
-          alert('Необходимо ввести имя игрока');
-        }
-      }
+      event.preventDefault();
+      upKeyHandler.apply(thisArg);
     } else if (event.keyCode === 39) {
-      if (thisArg.getGameUsername()) {
-        event.preventDefault();
-        rightKeyHandler.apply(thisArg);
-      } else {
-        if (thisArg.getGameUsernameInput() !== document.activeElement) {
-          alert('Необходимо ввести имя игрока');
-        }
-      }
+      event.preventDefault();
+      rightKeyHandler.apply(thisArg);
     } else if (event.keyCode === 40) {
-      if (thisArg.getGameUsername()) {
-        event.preventDefault();
-        downKeyHandler.apply(thisArg);
-      } else {
-        if (thisArg.getGameUsernameInput() !== document.activeElement) {
-          alert('Необходимо ввести имя игрока');
-        }
-      }
+      event.preventDefault();
+      downKeyHandler.apply(thisArg);
     } else {
       return;
     }
+  })
+}
+
+function setMouseMoveHandler(leftKeyHandler: Function, upKeyHandler: Function, rightKeyHandler: Function, downKeyHandler: Function, thisArg: Board): void {
+  const mouseDownPosition: { x: number, y: number } = { x: 0, y: 0 };
+  const mouseUpPosition: { x: number, y: number } = { x: 0, y: 0 };
+
+  document.addEventListener('mousedown', (e) => {
+    mouseDownPosition.x = e.x;
+    mouseDownPosition.y = e.y;
   });
+
+  document.addEventListener('mouseup', (e) => {
+    mouseUpPosition.x = e.x;
+    mouseUpPosition.y = e.y;
+    let diffX: number = Math.abs(mouseUpPosition.x - mouseDownPosition.x);
+    let diffY: number = Math.abs(mouseUpPosition.y - mouseDownPosition.y);
+
+    if (diffX >= diffY) {
+      if (mouseDownPosition.x > mouseUpPosition.x) {
+        leftKeyHandler.apply(thisArg);
+      } else if (mouseDownPosition.x < mouseUpPosition.x) {
+        rightKeyHandler.apply(thisArg);
+      } else {
+        return;
+      }
+    } else {
+      if (mouseDownPosition.y > mouseUpPosition.y) {
+        upKeyHandler.apply(thisArg);
+      } else if (mouseDownPosition.y < mouseUpPosition.y) {
+        downKeyHandler.apply(thisArg);
+      } else {
+        return;
+      }
+    }
+  });
+
+}
+
+function setTouchHandler(leftKeyHandler: Function, upKeyHandler: Function, rightKeyHandler: Function, downKeyHandler: Function, thisArg: Board): void {
+  const touchStartPosition: { x: number, y: number } = { x: 0, y: 0 };
+  const touchEndPosition: { x: number, y: number } = { x: 0, y: 0 };
+
+  document.addEventListener('touchstart', (e) => {
+    touchStartPosition.x = e.changedTouches[0].clientX;
+    touchStartPosition.y = e.changedTouches[0].clientY;
+  });
+
+  document.addEventListener('touchend', (e) => {
+    touchEndPosition.x = e.changedTouches[0].clientX;
+    touchEndPosition.y = e.changedTouches[0].clientY;
+    let diffX: number = Math.abs(touchEndPosition.x - touchStartPosition.x);
+    let diffY: number = Math.abs(touchEndPosition.y - touchStartPosition.y);
+
+    if (diffX >= diffY) {
+      if (touchStartPosition.x > touchEndPosition.x) {
+        leftKeyHandler.apply(thisArg);
+      } else if (touchStartPosition.x < touchEndPosition.x) {
+        rightKeyHandler.apply(thisArg);
+      } else {
+        return;
+      }
+    } else {
+      if (touchStartPosition.y > touchEndPosition.y) {
+        upKeyHandler.apply(thisArg);
+      } else if (touchStartPosition.y < touchEndPosition.y) {
+        downKeyHandler.apply(thisArg);
+      } else {
+        return;
+      }
+    }
+  });
+
 }
 
 function getTopRightIndex(array: any, from: number): number {
@@ -98,7 +146,7 @@ function getLeftBottomIndex(array: any, from: number): number {
   return -1;
 }
 
-function updateMapCoordinates(currentPosition: { x: number, y: number }, newPosition: { x: number, y: number }, map: Array<string>) {
+function updateMapCoordinates(currentPosition: { x: number, y: number }, newPosition: { x: number, y: number }, map: Array<string>): void {
   if (currentPosition.x === newPosition.x && currentPosition.y === newPosition.y) {
     return;
   }
@@ -109,6 +157,8 @@ function updateMapCoordinates(currentPosition: { x: number, y: number }, newPosi
 
 export {
   setKeyDownHandler,
+  setMouseMoveHandler,
+  setTouchHandler,
   getTopRightIndex,
   getTopLeftIndex,
   getLeftTopIndex,
