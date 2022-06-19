c2c1db784c7577c30a19e3c66055a4e026183834
gruv19 <grv1992@yandex.ru>
2022-06-19 13:08:59 +0500

    feat: Добавил анимацию с помощью AOS

--- module4/src/components/article-card/article-card.js
+++ module4/src/components/article-card/article-card.js
@@ -1,10 +1,20 @@
 import './article-card.scss';
 import articleCardemplate from './article-card.template';
+import AOS from 'aos';
+import '../../../node_modules/aos/dist/aos.css';
 
 const articleCard = (content, type = 'default') => {
   const temporaryElement = document.createElement('div');
   temporaryElement.innerHTML = articleCardemplate(content, type);
   const articleCard = temporaryElement.firstElementChild;
+
+  AOS.init();
+  if (type === 'default') {
+    articleCard.setAttribute('data-aos', 'zoom-in');
+  } else {
+    articleCard.setAttribute('data-aos', 'flip-left');
+    articleCard.setAttribute('data-aos-duration', '1000');
+  }
 
   return articleCard;
 };
